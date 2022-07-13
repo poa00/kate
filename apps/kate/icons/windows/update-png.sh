7c8c8c2ad8363d9d9e526c1bab584063d11098e9
Eric Armbruster <eric1@armbruster-online.de>
2022-07-13 18:35:05 +0200

    windows: add copy of Kate icon for unplating
    https://docs.microsoft.com/en-us/windows/msix/desktop/desktop-to-uwp-manual-conversion#optional-add-target-based-unplated-assets
    and https://github.com/MicrosoftDocs/windows-uwp/issues/1827 suggest adding
    a copy of the logo to the same folder to achieve unplating in the start menu
    and other places.
    
    BUG: 442761

--- apps/kate/icons/windows/update-png.sh
+++ apps/kate/icons/windows/update-png.sh
@@ -1,3 +1,5 @@
 for i in 16 22 32 44 48 64 128 150 256 310 512; do
     ksvgtopng5 $i $i sc-apps-kate.svg $i-apps-kate.png
+    # BUG: 442761, see https://docs.microsoft.com/en-us/windows/msix/desktop/desktop-to-uwp-manual-conversion#optional-add-target-based-unplated-assets
+    cp $i-apps-kate.png $i-apps-kate.targetsize-${i}_altform-unplated.png
 done
